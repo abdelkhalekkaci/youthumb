190185638eb5bf65edbed088642d984a8494ae2c
abdelkhalekkaci <kaci.abdelkhalek.95@gmail.com>
2023-11-21 12:02:22 +0100

    a

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
+import copy from 'copy-to-clipboard';
 
 const Index = () => {
     const [videoURL, setVideoURL] = useState('');
     const [thumbnailOptions, setThumbnailOptions] = useState([]);
-    const [error, setError] = useState(null);
 
     const getYouTubeThumbnail = (url) => {
         let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
@@ -28,30 +28,53 @@ const Index = () => {
 
             setThumbnailOptions(thumbnailOptions);
             setVideoURL('');
-            setError(null); // Clear previous errors if any
         } else {
             setThumbnailOptions([]);
         }
     };
 
-    const downloadThumbnail = async (url) => {
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const blob = await response.blob();
-            const blobURL = URL.createObjectURL(blob);
-            const anchor = document.createElement('a');
-            anchor.href = blobURL;
-            anchor.download = 'thumbnail.jpg';
-            anchor.click();
-        } catch (error) {
-            setError('Error downloading thumbnail. Please try again.'); // Set error state
-            console.error('Error downloading thumbnail:', error);
-        }
+    const downloadThumbnail = (url) => {
+        const anchor = document.createElement('a');
+        anchor.href = url;
+        anchor.download = 'thumbnail.jpg';
+        anchor.click();
     };
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <header className="text-center 
+            <header className="text-center mb-8">
+                <h1 className="text-3xl font-bold mb-2">Youtube Thumbnail Downloader</h1>
+                <p className="text-gray-600">Download high-quality thumbnails from YouTube videos.</p>
+            </header>
+            <div className="text-center">
+                <input
+                    type="text"
+                    className="w-full md:w-1/2 px-4 py-2 border rounded"
+                    placeholder="Enter YouTube URL"
+                    value={videoURL}
+                    onChange={(e) => setVideoURL(e.target.value)}
+                />
+                <button className="btn-blue mt-2" onClick={() => getYouTubeThumbnail(videoURL)}>
+                    Download Thumbnails
+                </button>
+            </div>
+            {thumbnailOptions.length > 0 && (
+                <div className="mt-8">
+                    <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
+                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                        {thumbnailOptions.map((option, index) => (
+                            <div key={index} className="thumbnail-option">
+                                <img src={option.url} alt={`Thumbnail ${index + 1}`} />
+                                <button className="btn-blue mt-2" onClick={() => downloadThumbnail(option.url)}>
+                                    Download Image
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Index;
