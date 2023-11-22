993d322d9a244caafb42834ec0db1da5b5e4fafb
abdelkhalekkaci <kaci.abdelkhalek.95@gmail.com>
2023-11-22 12:35:04 +0100

    a

--- pages/index.js
+++ pages/index.js
@@ -26,40 +26,34 @@ const Index = () => {
     };
 
     return (
-        <div className="container mx-auto px-4 py-8 flex flex-col items-center">
+        <div className="container">
             <header className="text-center mb-8">
                 <h1 className="text-3xl font-bold mb-2">Youtube Thumbnail Downloader</h1>
                 <p className="text-gray-600 intro-text">Download high-quality thumbnails from YouTube videos.</p>
             </header>
 
-            <div className="flex flex-col items-center mb-8">
+            <div className="center-content">
                 <input
                     type="text"
-                    className="w-full md:w-1/2 px-4 py-2 border rounded"
+                    className="input-field"
                     placeholder="Enter YouTube URL"
                     value={videoURL}
                     onChange={(e) => setVideoURL(e.target.value)}
                 />
-                <br />
-                <button
-                    className="btn-blue mt-2"
-                    onClick={handleDownloadClick}
-                >
+                <button className="btn-blue" onClick={handleDownloadClick}>
                     {thumbnailOptions.length === 0 ? 'Download Thumbnails' : 'Processing...'}
                 </button>
             </div>
 
             {thumbnailOptions.length > 0 && (
-                <div className="mt-8">
+                <div className="thumbnail-options">
                     {/* Render thumbnail options */}
                 </div>
             )}
 
-            <div className="flex-grow" />
-
-            <section className="mt-8 flex flex-col items-center">
-                <h2 className="text-2xl font-semibold mb-4 text-center">How to Use Our Website</h2>
-                <p className="text-gray-700 text-center">
+            <section className="how-to-use">
+                <h2 className="how-to-use-header">How to Use Our Website</h2>
+                <p className="how-to-use-text">
                     To download a thumbnail,<br />
                     First, enter a valid YouTube video URL in the input field above and click the "Download Thumbnails" button.<br />
                     Once the thumbnail options appear below, click the "Download Image" button below the desired thumbnail to start the download.<br />
