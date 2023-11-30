816c57488f37b950ebb985ffe62ba2049a8847cd
abdelkhalekkaci <kaci.abdelkhalek.95@gmail.com>
2023-11-30 12:04:32 +0100

    r

--- pages/index.js
+++ pages/index.js
@@ -37,10 +37,10 @@ const Index = () => {
             const response = await fetch(url);
             const blob = await response.blob();
 
-            const link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            link.download = 'thumbnail.jpg';
-            link.click();
+            const anchor = document.createElement('a');
+            anchor.href = window.URL.createObjectURL(blob);
+            anchor.download = 'thumbnail.jpg';
+            anchor.click();
         } catch (error) {
             console.error('Error downloading thumbnail:', error);
         }
