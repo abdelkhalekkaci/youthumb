ef6c1b25d6cf4e0e2378932f538a95ef4fdaef77
abdelkhalekkaci <kaci.abdelkhalek.95@gmail.com>
2024-03-31 12:34:37 +0000

    r

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import { DefaultSeo } from "next-seo";
 import Header from './Header';
 
 function MyApp({ Component, pageProps }) {
-HEAD
     return (
         <Fragment>
             <DefaultSeo
@@ -22,24 +21,6 @@ HEAD
             <Component {...pageProps} />
         </Fragment>
     );
-
-  return (
-    <Fragment>
-      <DefaultSeo
-        title="Youtube Thumbnail Downloader"
-        description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://your-website-url.com"
-        openGraph={{
-          url: "https://your-website-url.com",
-          title: "Youtube Thumbnails Downloader",
-          description: "Download high-quality thumbnails from YouTube videos.",
-          site_name: "Youtube Thumbnail Downloader",
-        }}
-      />
-      <Component {...pageProps} />
-    </Fragment>
-  );
-c0bae8814279df52ad1c80f7d3d5764faef3b0b6
 }
 
 export default MyApp;
