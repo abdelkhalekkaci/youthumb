c379596870bbefcac545d377a911ee3bf4973ec9
abdelkhalekkaci <kaci.abdelkhalek.95@gmail.com>
2024-04-03 22:34:30 +0000

    e

--- pages/ContactUs.js
+++ pages/ContactUs.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import Head from 'next/head'; // Import Head component from next/head
+import Header from './Header';
+import styles from '../styles/Home.module.css';
 
 const ContactUs = () => {
     return (
-        <div>
-            <h1>Contact Us</h1>
-            <p>This is the contact page content.</p>
+        <div className={styles.container} style={{ backgroundColor: '#EFF7F6' }}>
+            <Head>
+                <title>Contact Us - Your Website Title</title>
+                <meta name="description" content="Contact Us page description goes here." />
+            </Head>
+            <Header /> {/* Include the Header component */}
+            <main className={styles.main}>
+                <h1>Contact Us</h1>
+                <p>This is the Contact Us page content.</p>
+            </main>
         </div>
     );
 };
 
 export default ContactUs;
-// JavaScript source code
