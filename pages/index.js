f733567012248ff6f2bb375b25fee285dbb62a70
abdelkhalekkaci <kaci.abdelkhalek.95@gmail.com>
2024-04-29 13:11:46 +0100

    e

--- pages/index.js
+++ pages/index.js
@@ -127,6 +127,16 @@ const Index = () => {
                     )}
                 </div>
             )}
+            <script type="text/javascript">
+                atOptions = {
+                    'key' : 'c8bdf251025782d8b42d28cfb61528ea',
+                'format' : 'iframe',
+                'height' : 60,
+                'width' : 468,
+                'params' : { }
+	};
+            </script>
+            <script type="text/javascript" src="//www.topcreativeformat.com/c8bdf251025782d8b42d28cfb61528ea/invoke.js"></script>
             <section
                 className="how-to-section"
                 style={{ backgroundColor: '#FFFFFF', padding: '20px', borderRadius: '8px', marginTop: '20px', textAlign: 'center' }}
